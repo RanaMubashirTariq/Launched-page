1eb6acf57ad81b7fb2b57678024e63d706abf660
Rana Mubashir Tariq <mubashirtariq.1366@gmail.com>
2025-09-22 14:16:15 +0500

    add all files3

--- src/screens/Desktop/sections/SecurityTrustSection/SecurityTrustSection.tsx
+++ src/screens/Desktop/sections/SecurityTrustSection/SecurityTrustSection.tsx
@@ -5,7 +5,7 @@ import { Card, CardContent } from "../../../../components/ui/card";
 import { Progress } from "../../../../components/ui/progress";
 import { Separator } from "../../../../components/ui/separator";
 import icPinksale from "../../../../../public/ic-pinksale-61500ae2-svg.svg"
-import frame from "../../../../../public/frame-16.svg"
+import framet from "../../../../../Public/frame-16.svg"
 import bellIcon from "../../../../../public/bell-icon.svg"
 export const SecurityTrustSection = (): JSX.Element => {
   // Card data for mapping
@@ -132,7 +132,7 @@ const LaunchpadCard = ({ groupImageSuffix }: { groupImageSuffix: string }) => {
 
             <div className="flex items-center gap-3 max-[680px]:gap-1">
               <div className="flex items-end gap-[11px] max-[680px]:gap-1">
-                <img className="w-[33px] max-[680px]:w-[23px]" alt="Frame" src={frame} />
+                <img className="w-[33px] max-[680px]:w-[23px]" alt="Frame" src={framet} />
                 <img className="w-[33px] max-[680px]:w-[23px]" alt="Frame" src={bellIcon} />
 
               </div>
